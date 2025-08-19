1e691407357bc555a137154af74186a18e471854
auto-commit <auto@example.com>
2025-08-19 15:40:08 +0200

    Vendor LCZ4r source into standalone repo

---
+++ greater_tunis_lcz_repo/LCZ4r/R/lcz_cal_area.R
@@ -0,0 +1,313 @@
+#' Calculate LCZ areas
+#'
+#' This function calculates the areas of LCZ classes in both percentage and square kilometers.
+#'
+#' @param x A raster SpatRaster layer containing LCZ classes.
+#' @param plot_type Type of plot to generate: "bar", "pie", or "donut". Default is "bar".
+#' @param iplot Logical, indicating whether to create a plot (default is TRUE).
+#' @param isave Logical, whether to save the plot to your directory (default is FALSE).
+#' @param save_extension File format for saving the plot. Options: "png", "jpg", "jpeg", "tif", "pdf", "svg" (default is "png").
+#' @param show_legend Logical. If TRUE, displays the legend on the plot. If FALSE, the legend will be hidden. Default is TRUE.
+#' @param inclusive Logical. If TRUE, use a colorblind-friendly palette. Default is FALSE.
+#' @param xlab Character, label for the x-axis (default is "LCZ code").
+#' @param ylab Character, label for the y-axis (default is "Area (km2)").
+#' @param ... Additional arguments to modify axis, legend, and plot labels, including title, subtitle, and caption.
+#'
+#' @return A summary table of LCZ class areas if plot = FALSE, otherwise, the specified plot in ggplot format.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Calculate LCZ area and visualize as bar plot
+#' lcz_cal_area(my_lcz_map, plot_type = "bar")
+#'
+#' # Visualize as pie chart
+#' lcz_cal_area(my_lcz_map, plot_type = "pie")
+#'
+#' # Save donut chart plot
+#' lcz_cal_area(my_lcz_map, plot_type = "donut", isave = TRUE)
+#'
+#' # Return a dataframe
+#' lcz_cal_area(my_lcz_map, plot_type = "donut", iplot = FALSE)
+#' }
+#' @importFrom rlang .data
+#'
+#' @seealso
+#' See the documentation for lcz_get_map() to obtain an LCZ map.
+
+lcz_cal_area <- function(x,
+                         plot_type = "bar",
+                         iplot = TRUE,
+                         isave = FALSE,
+                         save_extension = "png",
+                         show_legend = TRUE,
+                         inclusive = FALSE,
+                         xlab = "LCZ code",
+                         ylab = "Area [km2]",
+                         ...) {
+  # Validate inputs ---------------------------------------------------------
+
+  if (!inherits(x, "SpatRaster")) {
+    x <- terra::rast({{ x }})
+  }
+
+  if (terra::nlyr(x) > 1) {
+    x <- x[[2]]
+  }
+
+  # Calculate raster area ---------------------------------------------------
+
+  freq_df <- tibble::as_tibble(terra::freq({{ x }}, bylayer = FALSE, usenames = TRUE)) %>%
+    dplyr::rename(lcz=value) %>%
+    dplyr::mutate(lcz = base::as.factor(lcz))
+
+  if (base::any((freq_df$lcz == 0))) {
+    freq_df$lcz[freq_df$lcz == 0] <- 17
+
+    freq_df <- dplyr::group_by(freq_df, lcz) %>%
+      dplyr::summarise(count = sum(.data$count)) %>%
+      dplyr::ungroup()
+  }
+
+  lcz_area <- terra::cellSize({{ x }}, unit = "km")
+
+  lcz_areas_df <- base::data.frame(
+    LCZ = terra::values({{ x }}),
+    Area_Km2 = terra::values(lcz_area)) %>%
+    stats::na.omit() %>%
+    dplyr::group_by(lcz) %>%
+    dplyr::summarise(area_km2 = base::round(base::sum(.data$area), digits = 2)) %>%
+    dplyr::mutate(lcz = base::as.factor(lcz))
+
+  summary_resul <- dplyr::inner_join(freq_df, lcz_areas_df, by = "lcz") %>%
+    dplyr::mutate(area_perc = base::round(.data$area_km2 / base::sum(.data$area_km2) * 100, digits = 2))
+
+  lcz <- c(base::seq(1, 10, 1), base::seq(11, 17)) %>%
+    tibble::as_tibble() %>%
+    dplyr::rename(ID = .data$value)
+
+  lcz.name <- c(
+    "Compact highrise", "Compact midrise", "Compact lowrise", "Open highrise",
+    "Open midrise", "Open lowrise", "Lightweight low-rise", "Large lowrise",
+    "Sparsely built", "Heavy Industry", "Dense trees", "Scattered trees",
+    "Bush, scrub", "Low plants", "Bare rock or paved", "Bare soil or sand", "Water"
+  ) %>%
+    tibble::as_tibble() %>%
+    dplyr::rename(lcz.name = .data$value)
+
+  lcz.col <- c(
+    "#910613", "#D9081C", "#FF0A22", "#C54F1E", "#FF6628", "#FF985E",
+    "#FDED3F", "#BBBBBB", "#FFCBAB", "#565656", "#006A18", "#00A926",
+    "#628432", "#B5DA7F", "#000000", "#FCF7B1", "#656BFA"
+  ) %>%
+    tibble::as_tibble() %>%
+    dplyr::rename(lcz.col = .data$value)
+
+  lcz_colorblind <- c(
+    "#E16A86", "#D8755E", "#C98027", "#B48C00",
+    "#989600", "#739F00", "#36A631", "#00AA63",
+    "#00AD89", "#00ACAA", "#00A7C5", "#009EDA",
+    "#6290E5", "#9E7FE5", "#C36FDA", "#D965C6",
+    "#E264A9"
+  ) %>%
+    tibble::as_tibble() %>%
+    dplyr::rename(lcz_colorblind = .data$value)
+
+  lcz_df <- dplyr::bind_cols(lcz, lcz.name, lcz.col, lcz_colorblind) %>%
+    dplyr::mutate(lcz = .data$ID) %>%
+    dplyr::mutate(lcz = base::as.factor(lcz)) %>%
+    dplyr::inner_join(summary_resul, by = "lcz")
+
+  # Define qualitative palette
+  if (inclusive == TRUE) {
+    color_values <- lcz_df %>%
+      dplyr::select(lcz, lcz_colorblind) %>%
+      dplyr::pull(lcz_colorblind, lcz)
+  } else {
+    color_values <- lcz_df %>%
+      dplyr::select(lcz, lcz.col) %>%
+      dplyr::pull(lcz.col, lcz)
+  }
+
+  # Define LCZ labels
+  lcz.lables <- lcz_df$lcz.name
+
+  if (plot_type == "bar") {
+    # Create the ggplot
+    graph <-
+      ggplot2::ggplot(lcz_df, ggplot2::aes(x = factor(lcz), y = .data$area_km2, fill = factor(lcz))) +
+      ggplot2::geom_bar(stat = "identity") +
+      ggplot2::scale_fill_manual(
+        values = color_values, name = "LCZ class",
+        labels = lcz.lables,
+        guide = ggplot2::guide_legend(
+          reverse = FALSE,
+          title.position = "top"
+        )
+      ) +
+      ggplot2::geom_text(
+        data = lcz_df,
+        label = paste0(round(lcz_df$area_perc, 1), "%"),
+        vjust = -0.2, size = 5, check_overlap = TRUE
+      ) +
+      ggplot2::scale_y_continuous(limits = c(0, base::max(lcz_df$area_km2) + 50)) +
+      ggplot2::labs(...,
+        x = xlab,
+        y = ylab
+      ) +
+      ggplot2::theme_bw() +
+      ggplot2::theme(
+        plot.title = ggplot2::element_text(color = "black", size = 18, face = "bold", hjust = 0.5),
+        plot.subtitle = ggplot2::element_text(color = "black", size = 17, hjust = 0.5),
+        plot.background = ggplot2::element_rect(fill="transparent", color=NA),
+        legend.background = ggplot2::element_rect(fill="transparent", color=NA),
+        panel.background = ggplot2::element_rect(color = NA, fill = "grey97"),
+        panel.grid.major = ggplot2::element_line(color = "grey90"),
+        panel.grid.minor = ggplot2::element_line(color = "grey90"),
+        panel.grid.major.y = ggplot2::element_line(color = "grey90"),
+        axis.text.x = ggplot2::element_text(size = 16),
+        axis.title.x = ggplot2::element_text(size = 16, face = "bold"),
+        axis.text.y = ggplot2::element_text(size = 16),
+        axis.title.y = ggplot2::element_text(size = 16, face = "bold"),
+        legend.text = ggplot2::element_text(size = 16),
+        legend.title = ggplot2::element_text(size = 17),
+        legend.spacing.y = ggplot2::unit(0.02, "cm"),
+        plot.margin = ggplot2::margin(25, 25, 10, 25),
+        plot.caption = ggplot2::element_text(color = "grey40", size = 10, hjust = 0)
+      )
+
+
+    if (!show_legend) {
+      graph <- graph + ggplot2::theme(legend.position = "none")
+    }
+  }
+
+  if (plot_type == "pie") {
+    # Compute a good label
+    lcz_df$label <- base::paste0(
+      base::round(lcz_df$area_perc, 1), "\u0025\n"
+    )
+    graph <-
+      ggplot2::ggplot(lcz_df, ggplot2::aes(x = "",
+                                           y = .data$area_perc,
+                                           fill = lcz,
+                                           label = .data$label)) +
+      ggplot2::geom_bar(stat = "identity", width = 1, color = NA, lwd = 0.2) +
+      ggplot2::coord_polar("y") +
+      ggplot2::scale_fill_manual(values = color_values, name = "LCZ class", labels = lcz.lables)+
+      ggplot2::geom_text(ggplot2::aes(label = .data$label),
+                         position = ggplot2::position_stack(vjust = 0.5),
+                         size = 5,check_overlap = TRUE, color = "white") +
+      ggplot2::theme_void() +
+      ggplot2::labs(...) +
+      # Add the labels
+      ggplot2::annotate("text", x = 1, y = 1.8, label = base::paste0("Total: ", base::sum(lcz_df$area_km2), " km\u00B2"),
+                        color = "black", size = 9, fontface = "bold", vjust = 21) +
+      ggplot2::theme(
+        plot.title =  ggplot2::element_text(hjust = 0.5, size = 16, face = "bold"),
+        plot.subtitle =  ggplot2::element_text(hjust = 0.5, size = 14),
+        legend.text = ggplot2::element_text(size = 14),
+        legend.title = ggplot2::element_text(size = 14,face = "bold"),
+        plot.caption = ggplot2::element_text(color = "grey40", size = 10, hjust = 1)
+      ) +
+      ggplot2::guides(fill = ggplot2::guide_legend(byrow = TRUE))
+
+    if (!show_legend) {
+      graph <- graph + ggplot2::theme(legend.position = "none")
+    }
+  }
+
+  if (plot_type == "donut") {
+    # Compute a good label
+    lcz_df$label <- base::paste0(
+      base::round(lcz_df$area_perc, 1), "\u0025\n"
+    )
+    graph <-
+      ggplot2::ggplot(lcz_df, ggplot2::aes(x = "",
+                                           y = .data$area_perc,
+                                           fill = lcz,
+                                           label = .data$label)) +
+      ggplot2::geom_bar(stat = "identity", width = 1, color = NA, lwd = 0.2) +
+      ggplot2::coord_polar("y") +
+      ggplot2::scale_fill_manual(values = color_values, name = "LCZ class", labels = lcz.lables)+
+      ggplot2::geom_text(ggplot2::aes(label = .data$label),
+                         position = ggplot2::position_stack(vjust = 0.5),
+                         size = 5, check_overlap = TRUE, color = "white") +
+      ggplot2::theme_void() +
+      ggplot2::labs(...) +
+      # Add the labels
+      ggplot2::annotate("text", x = -0.5, y = 0, label = base::paste0("Total:\n", base::sum(lcz_df$area_km2), " km\u00B2"),
+                        color = "black", size = 9, fontface = "bold", vjust = 0.5) +
+      ggplot2::theme(
+        plot.title =  ggplot2::element_text(hjust = 0.5, size = 16, face = "bold"),
+        plot.subtitle =  ggplot2::element_text(hjust = 0.5, size = 14),
+        legend.text = ggplot2::element_text(size = 14),
+        legend.title = ggplot2::element_text(size = 14,face = "bold"),
+        plot.caption = ggplot2::element_text(color = "grey40", size = 10, hjust = 0)
+      ) +
+      ggplot2::guides(fill = ggplot2::guide_legend(byrow = TRUE))
+
+    if (!show_legend) {
+      graph <- graph + ggplot2::theme(legend.position = "none")
+    }
+  }
+
+  if (isave == TRUE && plot_type == "bar") {
+    folder <- base::paste0("LCZ4r_output/")
+    if (!base::dir.exists(folder)) {
+      base::dir.create(folder)
+    }
+    file.1 <- base::paste0(getwd(), "/", folder, "lcz4r_area_bar.", save_extension)
+    ggplot2::ggsave(file.1, graph,
+      height = 7, width = 12,
+      dpi = 600
+    )
+    file.2 <- base::paste0(getwd(), "/", folder, "lcz4r_area_df.csv")
+    utils::write.csv(lcz_df, file.2)
+    base::message("Looking at your files in the path:", base::paste0(
+      getwd(),
+      "/", folder
+    ))
+  }
+
+  if (isave == TRUE && plot_type == "pie") {
+    folder <- base::paste0("LCZ4r_output/")
+    if (!base::dir.exists(folder)) {
+      base::dir.create(folder)
+    }
+
+    file.1 <- base::paste0(getwd(), "/", folder, "lcz4r_area_pie.", save_extension)
+    ggplot2::ggsave(file.1, graph, height = 7, width = 9, dpi = 600)
+    file.2 <- base::paste0(getwd(), "/", folder, "lcz4r_area_df.csv")
+    utils::write.csv(lcz_df, file.2)
+    base::message("Looking at your files in the path:", base::paste0(
+      getwd(),
+      "/", folder
+    ))
+  }
+
+  if (isave == TRUE && plot_type == "donut") {
+    folder <- base::paste0("LCZ4r_output/")
+    if (!base::dir.exists(folder)) {
+      base::dir.create(folder)
+    }
+    file.1 <- base::paste0(getwd(), "/", folder, "lcz4r_area_donut.", save_extension)
+    ggplot2::ggsave(file.1, graph, scale = 1.5,
+      height = 7, width = 9,
+      dpi = 600
+    )
+    file.2 <- base::paste0(getwd(), "/", folder, "lcz4r_area_df.csv")
+    utils::write.csv(lcz_df, file.2)
+    base::message("Looking at your files in the path:", base::paste0(
+      getwd(),
+      "/", folder
+    ))
+  }
+
+  if (iplot == FALSE) {
+    return(lcz_df)
+  } else {
+    return(graph)
+  }
+}
---
+++ greater_tunis_lcz_repo/LCZ4r/R/lcz_get_map.R
@@ -0,0 +1,166 @@
+
+#' Download the LCZ map
+#'
+#' This function retrieves the Local Climate Zone (LCZ) global mapping dataset
+#' It allows you to obtain the LCZ map for a specific area of interest, which can be a city, state, region, or custom-defined shape.
+#'
+#' @param city A character string specifying the name of your target area based on the OpenStreetMap project.
+#' @param roi Optionally, you can provide a Region of Interest (ROI) in ESRI shapefile format to clip the LCZ map to a custom area.
+#' @param isave_map Logical. Set to TRUE if you wish to save the resulting clipped map as a raster TIFF file on your local machine.
+#' @param isave_global Logical. Set to TRUE if you wish to save the global LCZ map as a raster TIFF file on your local machine.
+#' @return A Terra raster TIFF file containing LCZ classes (1-17).
+#'
+#' @export
+#'
+#' @references
+#' Demuzere, M., Kittner, J., Martilli, A., Mills, G., Moede, C., Stewart, I. D., van Vliet, J., and Bechtel, B. (2022). A global map of Local Climate Zones to support earth system modelling and urban scale environmental science, Earth Syst. Sci. Data 14(8) 3835-3873. DOI:https://doi.org/10.5194/essd-14-3835-2022
+#' Zenodo. https://doi.org/10.5281/zenodo.6364594
+#' Stewart ID, Oke TR. (2012). Local Climate Zones for Urban Temperature Studies. Bull Am Meteorol Soc. 93(12):1879-1900. doi:10.1175/BAMS-D-11-00019.1
+#'
+#' @examples
+#' \dontrun{
+#' # Load the LCZ map for a city
+#' my_lcz_city <- lcz_get_map(city = "your city")
+#'
+#' # Get LCZ map for a custom region of interest
+#' custom_roi <- sf::st_read("custom_roi.shp")
+#' roi_lcz <- lcz_get_map(roi = custom_roi, isave_map = TRUE)
+#'
+#' # Retrieve the LCZ map for a country
+#' my_lcz_country <- lcz_get_map(city = "Brazil")
+#' }
+#'
+#' @keywords LCZ, Local Climate Zone, urban climate, spatial analysis
+
+lcz_get_map <- function(city = NULL, roi = NULL, isave_map = FALSE, isave_global = FALSE) {
+  # Validate inputs
+  if (is.null(city) & is.null(roi)) {
+    stop("Error: provide either a city name or a roi polygon")
+  }
+
+  if (!is.null(city)) {
+    # Get study area polygon from OpenStreetMap data
+    shp_verify <- osmdata::getbb({{ city }}, format_out = "sf_polygon", limit = 1)
+
+    if (is.null(shp_verify)) {
+      stop(paste0("No polygonal boundary for", city, ".See https://nominatim.openstreetmap.org"))
+    }
+    # Check if polygon was obtained successfully
+    if (!is.null(shp_verify$geometry) & !inherits(shp_verify, "list")) {
+      study_area <- shp_verify$geometry
+      study_area <- sf::st_make_valid(study_area) %>%
+        sf::st_as_sf() %>%
+        sf::st_transform(crs = "+proj=longlat +datum=WGS84 +no_defs")
+    } else {
+      study_area <- shp_verify$multipolygon
+      study_area <- sf::st_make_valid(study_area) %>%
+        sf::st_as_sf() %>%
+        sf::st_transform(crs = "+proj=longlat +datum=WGS84 +no_defs")
+    }
+    options(warn = -1)
+    # Download the LCZ global map
+    lcz_url <- "https://zenodo.org/records/8419340/files/lcz_filter_v3.tif?download=1"
+    lcz_download <- terra::rast(base::paste0("/vsicurl/", lcz_url))
+
+    if (base::is.null(lcz_download)) {
+      stop("This error might be due to server restrictions. In such cases, you may need to download the file manually using this link:
+           https://zenodo.org/records/8419340/files/lcz_filter_v3.tif?download=1.
+           Then read it using the terra package, eg., my_map <- rast('path/lcz_filter_v3.tif'), and use the function lcz_get_map2()")
+    }
+
+    lcz_ras <- terra::crop(lcz_download, terra::ext(study_area))
+
+    if (is.null(lcz_ras)) {
+      stop("Large Data: If you are working with very large raster datasets, consider working on a
+           subset of the data to reduce the memory and processing requirements.
+           You can crop a smaller region first to see if the operation succeeds.")
+    }
+
+    lcz_ras <- terra::mask(lcz_ras, terra::vect(study_area))
+    base::names(lcz_ras) <- "lcz"
+
+    if (isave_map == TRUE) {
+      # Create a folder name using paste0
+      folder <- base::paste0("LCZ4r_output/")
+
+      # Check if the folder exists
+      if (!dir.exists(folder)) {
+        # Create the folder if it does not exist
+        base::dir.create(folder)
+      }
+
+      file <- base::paste0(getwd(), "/", folder, "lcz_map.tif")
+      terra::writeRaster(lcz_ras, file, overwrite = TRUE)
+      base::message("Looking at your files in the path:", base::paste0(getwd(), "/", folder))
+    }
+
+    if (isave_global == TRUE) {
+      # Create a folder name using paste0
+      folder <- base::paste0("LCZ4r_output/")
+
+      # Check if the folder exists
+      if (!base::dir.exists(folder)) {
+        # Create the folder if it does not exist
+        base::dir.create(folder)
+      }
+
+      file <- base::paste0(getwd(), "/", folder, "lcz_global_map.tif")
+      terra::writeRaster(lcz_download, file, overwrite = TRUE)
+      base::message("Looking at your files in the path:", base::paste0(getwd(), "/", folder))
+    }
+
+    return(lcz_ras)
+  } else {
+    # Download the LCZ global map from https://zenodo.org/record/6364594/files/lcz_filter_v1.tif?download=1
+    lcz_url <- "https://zenodo.org/records/8419340/files/lcz_filter_v3.tif?download=1"
+    lcz_download <- terra::rast(base::paste0("/vsicurl/", lcz_url))
+    roi_crs <- roi %>%
+      sf::st_as_sf() %>%
+      sf::st_make_valid() %>%
+      sf::st_transform(crs = 4326)
+
+    lcz_ras <- terra::crop(lcz_download, terra::ext(roi_crs))
+
+    if (is.null(lcz_ras)) {
+      stop("Large Data: If you are working with very large raster datasets, consider working on a
+           subset of the data to reduce the memory and processing requirements.
+           You can crop a smaller region first to see if the operation succeeds.")
+    }
+
+    lcz_ras <- terra::mask(lcz_ras, terra::vect(roi_crs))
+    base::names(lcz_ras) <- "lcz"
+
+    if (isave_map == TRUE) {
+      # Create a folder name using paste0
+      folder <- base::paste0("LCZ4r_output/")
+
+      # Check if the folder exists
+      if (!dir.exists(folder)) {
+        # Create the folder if it does not exist
+        base::dir.create(folder)
+      }
+
+      file <- base::paste0(getwd(), "/", folder, "lcz_map.tif")
+      terra::writeRaster(lcz_ras, file, overwrite = TRUE)
+      base::message("Looking at your files in the path:", base::paste0(getwd(), "/", folder))
+    }
+
+    if (isave_global == TRUE) {
+      # Create a folder name using paste0
+      folder <- base::paste0("LCZ4r_output/")
+
+      # Check if the folder exists
+      if (!base::dir.exists(folder)) {
+        # Create the folder if it does not exist
+        base::dir.create(folder)
+      }
+
+      file <- base::paste0(getwd(), "/", folder, "lcz_global_map.tif")
+      terra::writeRaster(lcz_download, file, overwrite = TRUE)
+      base::message("Looking at your files in the path:", base::paste0(getwd(), "/", folder))
+    }
+
+    return(lcz_ras)
+  }
+}
+
